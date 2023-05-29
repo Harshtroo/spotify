45db27305172aee8b8ec93ef9a8c7ab14827ce47
harsh <harsh.vekariya@trootech.com>
2023-05-29 18:39:36 +0530

    add email register

--- spotify__quixom/spotify_quixom/spotify/admin.py
+++ spotify__quixom/spotify_quixom/spotify/admin.py
@@ -1,5 +1,3 @@
-# Register your models here.
-
 from django.contrib import admin
 from .models import User
 # Register your models here.
--- spotify__quixom/spotify_quixom/spotify/models.py
+++ spotify__quixom/spotify_quixom/spotify/models.py
@@ -6,10 +6,6 @@ class User(AbstractUser):
     "this model use create user"
     mobile_number = models.IntegerField(validators=[MaxValueValidator(9999999999)],default=0)
 
-    # def save(self, *args, **kwargs):
-    #     self.password = make_password(self.password)
-    #     super(User, self).save(*args, **kwargs)
-
 class Song(models.Model):
     ROLES=(
         ("English","English"),
