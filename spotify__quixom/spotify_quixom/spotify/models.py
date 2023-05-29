f446eac3ee060d199cb0b0588e6d538c2f36b529
harsh <harsh.vekariya@trootech.com>
2023-05-29 17:58:48 +0530

    login register add

--- spotify__quixom/spotify_quixom/spotify/models.py
+++ spotify__quixom/spotify_quixom/spotify/models.py
@@ -1,12 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-# Create your models here.
-
-class User(AbstractUser):
-    "this model use create user"
-    username = models.CharField(max_length=100,unique=True)
-from django.db import models
-from django.contrib.auth.models import AbstractUser
 from django.core.validators import MaxValueValidator
 
 class User(AbstractUser):
@@ -17,15 +10,6 @@ class User(AbstractUser):
     #     self.password = make_password(self.password)
     #     super(User, self).save(*args, **kwargs)
 
-class Song(models.Model):
-    ROLES=(
-        ("English","English"),
-        ("Gujrati","Gujrati"),
-        ("Hindi","Hindi"),
-    )
-    name = models.CharField(max_length=100)
-    singer_name = models.CharField(max_length=100)
-    category = models.CharField(max_length=50,choices=ROLES, null=True)
 class Song(models.Model):
     ROLES=(
         ("English","English"),
