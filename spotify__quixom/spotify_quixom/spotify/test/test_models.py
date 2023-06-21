22299c409874d9b350458200d02c60481a45ba6d
Harshtroo <123172610+Harshtroo@users.noreply.github.com>
2023-06-22 00:02:15 +0530

    add

--- spotify__quixom/spotify_quixom/spotify/test/test_models.py
+++ spotify__quixom/spotify_quixom/spotify/test/test_models.py
@@ -1,17 +1,56 @@
 from django.test import TestCase, Client
 from spotify.models import User
-from django.core.validation import ValidationError
+from django.core.exceptions import ValidationError
 
 class UserTstCases(TestCase):
 
     def test_create_user(self):
-        user = User.objects.create(username="abhi",email="abhi@gmail.com",mobile_number="9874561230",role="singer",password="harsh@1234")
-        self.assertEquals(user.username,"abhi")
+        user = User.objects.create(username="abhi",email="abhi123@gmail.com",mobile_number=6355157752,role="singer")
+        user.set_password("abhi@1234")  
+        user.save()
+        self.assertEqual(user.username, "abhi")
+        self.assertEqual(user.email, "abhi123@gmail.com")
+        self.assertEqual(user.mobile_number, 6355157752)  
+        self.assertEqual(user.role, "singer")
+        self.assertTrue(user.check_password("abhi@1234"))
 
-    def test_required_fields(self):
-        # user = User.objects.create(username="abhi",email="abhi@gmail.com",mobile_number="9874561230",role="singer",password="harsh@1234")
-        user = User(username="abhi")
-        # with self.assertRaises(ValidationError):
+    # def test_all_field_required(self):
+    #     with self.assertRaises(ValidationError):
+    #         user = User.objects.create(email="abhi123@gmail.com", mobile_number=6355157752, role="singer")
+
+    #     with self.assertRaises(ValidationError):
+    #         user = User.objects.create(username="abhi", mobile_number=6355157752, role="singer")
+
+    #     with self.assertRaises(ValidationError):
+    #         user = User.objects.create(username="abhi", email="abhi123@gmail.com", role="singer")
+
+    #     with self.assertRaises(ValidationError):
+    #         user = User.objects.create(username="abhi", email="abhi123@gmail.com", mobile_number=6355157752)
+
+    def test_create_user_with_invalid_mobile_number(self):
+        with self.assertRaises(ValidationError) as context:
+            User.objects.create(username='abhi', mobile_number="123", role='singer')
+        self.assertEqual(context.exception.message_dict['mobile_number'][0], 'Ensure this value is greater than or equal to 0 and less than or equal to 9999999999.')
+
+    # def test_mobile_number_validator(self):
+    #     # Test the mobile number validator
+    #     with self.assertRaises(ValidationError):
+    #         user = User(
+    #             username="abhi",
+    #             email="abhi123@gmail.com",
+    #             mobile_number=123456789,  # Invalid mobile number, less than 10 digits
+    #             role="singer",
+    #             password="abhi@1234"
+    #         )
+    #         user.full_clean()  
+
+
+
+
+    # def test_required_fields(self):
+    #     # user = User.objects.create(username="abhi",email="abhi@gmail.com",mobile_number="9874561230",role="singer",password="harsh@1234")
+    #     user = User(username="abhi")
+    #     # with self.assertRaises(ValidationError):
 
 
 
